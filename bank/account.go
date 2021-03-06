2c1c026c04cc5b90dbf2277ab94e11a35ccedd98
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-03-06 14:29:21 +0700

    add transfer test

--- bank/account.go
+++ bank/account.go
@@ -8,6 +8,8 @@ type Account struct {
 }
 
 func (a *Account) Deposit(amount int) {
+	a.l.Lock()
+	defer a.l.Unlock()
 	a.balance += amount
 }
 
@@ -25,3 +27,17 @@ func (a *Account) Balance() int {
 	return a.balance
 }
 
+func (a *Account) Transfer(b *Account, amount int) bool {
+	a.l.Lock()
+	if a.balance < amount {
+		a.l.Unlock()
+		return false
+	}
+	a.balance -= amount
+	a.l.Unlock()
+
+	b.l.Lock()
+	b.balance += amount
+	b.l.Unlock()
+	return true
+}
--- bank/account_test.go
+++ bank/account_test.go
@@ -1,6 +1,7 @@
 package bank
 
 import (
+	"math/rand"
 	"sync"
 	"testing"
 )
@@ -24,6 +25,36 @@ func TestAccountWithRaceCondition(t *testing.T) {
 
 	balance := acc.Balance()
 	if balance != 0 {
+		t.Errorf("actual=%d", balance)
+	}
+}
+
+func TestAccountTransfer(t *testing.T) {
+	acc1 := &Account{}
+	acc1.Deposit(1000000)
+	acc2 := &Account{}
+	acc2.Deposit(1000000)
+	acc3 := &Account{}
+	acc3.Deposit(1000000)
+	accounts := []*Account{acc1, acc2, acc3}
+
+	wg := sync.WaitGroup{}
+	wg.Add(1000)
+	for i := 0; i < 1000; i++ {
+		from := accounts[rand.Int()%3]
+		to := accounts[rand.Int()%3]
+
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 1000; i++ {
+				from.Transfer(to, 1)
+			}
+		}()
+	}
+	wg.Wait()
+
+	balance := acc1.Balance()+acc2.Balance()+acc3.Balance()
+	if balance != 3000000 {
 		t.Errorf("actual=%d", balance)
 	}
 }
